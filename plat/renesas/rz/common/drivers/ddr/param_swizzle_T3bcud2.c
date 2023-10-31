215afc594ae3ebdb152840b090738e0d93807d81
Ilko Iliev <iliev@ronetix.at>
2023-10-31 15:25:58 +0100

    Modify to match the DDR configuration of Ronetix RNX-RZG2UL-OSM

--- plat/renesas/rz/common/drivers/ddr/param_swizzle_T3bcud2.c
+++ plat/renesas/rz/common/drivers/ddr/param_swizzle_T3bcud2.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020-2022, Renesas Electronics Corporation. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
- * This code was generated with RZ/G2L, G2UL, Five, A3UL DDR config generation tool v3.0.0
+ * This code was generated with RZ/G2L, G2UL, Five, A3UL DDR config generation tool v3.0.1
  */
 
 #include <stdint.h>
@@ -42,32 +42,32 @@ const uint32_t mc_phy_settings_tbl[MC_PHYSET_NUM][2] = {
 };
 
 const uint32_t swizzle_mc_tbl[SWIZZLE_MC_NUM][2] = {
-	{DDRMC_R030,	0x00000000	},
+	{DDRMC_R030,	0x0000000C	},
 	{DDRMC_R031,	0x00000000	},
-	{DDRMC_R032,	0x1E191117	},
-	{DDRMC_R033,	0x1D000A16	},
-	{DDRMC_R034,	0x000B0D12	},
-	{DDRMC_R035,	0x1B180C1A	},
-	{DDRMC_R036,	0x15101C1F	},
-	{DDRMC_R037,	0x00090E13	},
+	{DDRMC_R032,	0x13151117	},
+	{DDRMC_R033,	0x19000916	},
+	{DDRMC_R034,	0x181C1B0D	},
+	{DDRMC_R035,	0x0B1A1E12	},
+	{DDRMC_R036,	0x0114101D	},
+	{DDRMC_R037,	0x00000A0E	},
 	{DDRMC_R038,	0x00000000	}
 };
 
 const uint32_t swizzle_phy_tbl[SIZZLE_PHY_NUM][2] = {
 	{DDRPHY_R29,	0x00000000	},
-	{DDRPHY_R11,	0x0A020B0F	},
+	{DDRPHY_R11,	0x18091703	},
 	{DDRPHY_R29,	0x00000001	},
-	{DDRPHY_R11,	0x18050E14	},
+	{DDRPHY_R11,	0x16020E14	},
 	{DDRPHY_R29,	0x00000002	},
-	{DDRPHY_R11,	0x16080603	},
+	{DDRPHY_R11,	0x07010005	},
 	{DDRPHY_R29,	0x00000003	},
-	{DDRPHY_R11,	0x190D0717	},
+	{DDRPHY_R11,	0x19150D04	},
 	{DDRPHY_R29,	0x00000004	},
 	{DDRPHY_R11,	0x13121110	},
 	{DDRPHY_R29,	0x00000005	},
-	{DDRPHY_R11,	0x04090C00	},
+	{DDRPHY_R11,	0x1A080B0F	},
 	{DDRPHY_R29,	0x00000006	},
-	{DDRPHY_R11,	0x1B1A1501	},
+	{DDRPHY_R11,	0x1B0A0C06	},
 	{DDRPHY_R29,	0x00000007	},
 	{DDRPHY_R11,	0x001E1D1C	}
 };
